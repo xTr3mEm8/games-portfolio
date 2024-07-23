f3edc3dafd639cf15deebafce029a44829ca1687
Cris <maluscristian6@gmail.com>
2024-07-23 10:45:11 +0300

    First commit

---
+++ Assets/Scripts/DespawnOnHeight.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class DespawnOnHeight : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y <= -4) {
+            SceneManager.LoadScene("GameOver");
+            
+        var whisper = GameObject.Find("WhisperSource");
+        GameObject.Destroy(whisper);            
+        }
+    }
+}
---
+++ Assets/Scripts/DisplayLevel.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class DisplayLevel : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Text Level;
+
+    public static int curLevel = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Level.text = "Level " + curLevel.ToString();
+    }
+}
